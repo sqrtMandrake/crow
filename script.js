10aa1f3d978224a43c53632c6e5abfbd4195a5a1
sqrtMandrake <sqrtMandrake@tutamail.com>
2026-05-30 17:32:06 +0200

    randomized caw

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const crow = document.getElementById('crowtainer');
-const audio = new Audio('./resources/crow_call.mp3')
+const crow = document.getElementById('crowtainer')
+const caw0 = new Audio('./resources/crow_call.mp3')
+const caw1 = new Audio('./resources/caw1.mp3')
+const caw2 = new Audio('./resources/caw2.mp3')
+const caw3 = new Audio('./resources/caw3.mp3')
+const caws = [caw0, caw1, caw2, caw3]
+
 
 crow.addEventListener('click', () => {
-    audio.play();
+    const cawNo = Math.floor(Math.random() * caws.length);
+    caws[cawNo].play();
     console.log('caw');
 });
